ab810d8ac7dfaf26703f7d3f341dd36fbcbfdfb6
Babon Das <babon.it@gmail.com>
2025-02-23 12:44:18 +0530

    'test1'

--- LearnGitDemo/Controllers/HomeController.cs
+++ LearnGitDemo/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace LearnGitDemo.Controllers
         {
             return View();
         }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
